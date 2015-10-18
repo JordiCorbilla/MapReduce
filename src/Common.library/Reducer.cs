38c57c0565805835085bcbc027840240efe670f4
Jordi Corbilla <jordi.coll.corbilla@gmail.com>
2015-10-18 19:09:14 +0100

    First Commit for the project.

---
+++ src/Common.library/Reducer.cs
@@ -0,0 +1,199 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) 2015 Jordi Corbilla
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+//######################################
+// Counting words using MapReduce Approach 
+// http://en.wikipedia.org/wiki/MapReduce
+// @Author: Jordi Corbilla
+// @version: 1.0
+// @date: 23/03/2015
+//######################################
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CountingWordsConsole
+{
+    //Class that mimics the MapReduce functionality
+    //It will load the text in chunks of 100 words and
+    //process them in different threads and updating
+    //a global dictionary
+    public class Reducer
+    {
+        private readonly ConcurrentDictionary<string, int> _distinctWordList;
+        private readonly BlockingCollection<string> _wordChunks;
+        public int Numwords { get; set; }
+
+        //Constructor
+        public Reducer()
+        {
+            _distinctWordList = new ConcurrentDictionary<string, int>();
+            var concurrentBag = new ConcurrentBag<string>();
+            _wordChunks = new BlockingCollection<string>(concurrentBag);
+            Numwords = 0;
+        }
+
+        //Return a list of chunks
+        //Basic idea is that the file is being divided in chunks of 100 words and
+        //then parallely processed to increase performance
+        private IEnumerable<string> CreateChunks(string text, int chunkSize)
+        {
+            List<string> chunks = new List<string>();
+            int offset = 0;
+            while (offset < text.Length)
+            {
+                int size = Math.Min(chunkSize, text.Length - offset);
+                chunks.Add(text.Substring(offset, size));
+                offset += size;
+            }
+            return chunks;
+        }
+
+        //Method to clean up the words we are reading.
+        //I'm just filtering for digits or letters.
+        private void CleanUpWord(string[] words)
+        {
+            //StringBuilder wordList = new StringBuilder();
+            //List<StringBuilder> list = new List<StringBuilder>();
+            StringBuilder[] list = new StringBuilder[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+                list[i] = new StringBuilder();
+           // list.Add(new StringBuilder());
+
+            //Task[] tasks = new Task<int>[words.Length];
+            List<Task> tasks = new List<Task>();
+
+            //for (int i = 0; i < words.Length; i++)
+            //    tasks[i] = new Task();
+
+            int index = 0;
+            foreach (string word in words)
+            {
+                int i = index;
+                tasks.Add( 
+                    Task.Factory.StartNew(() =>
+                {
+                    //lock(index)
+                    //{
+                        LetterOrDigit(word, list[i]);
+                    //}
+                }));
+                index++;
+            }
+            Task.WaitAll(tasks.ToArray());
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i].Length > 0)
+                {
+                    _wordChunks.Add(list[i].ToString());
+                    list[i].Clear();
+                }
+            }
+
+            //foreach (string word in words)
+            //{   
+            //    //Remove unwanted characters
+            //    foreach (char c in word)
+            //    {
+            //        //We could add more filtering here as I'm just considering letters or digits.
+            //        if (char.IsLetterOrDigit(c))
+            //            wordList.Append(c);
+            //    }
+            //    //Send word to the wordChunks Blocking Collection
+            //    if (wordList.Length > 0)
+            //    {
+            //        _wordChunks.Add(wordList.ToString());
+            //        wordList.Clear();
+            //    }
+            //}            
+        }
+
+        private void LetterOrDigit(string word, StringBuilder sb)
+        {
+            foreach (char c in word)
+            {
+                //We could add more filtering here as I'm just considering letters or digits.
+                if (char.IsLetterOrDigit(c))
+                    sb.Append(c);
+            }
+        }
+
+        //Parse every chunk of text and perform a clean up operation.
+        //I'm replacing new lines also \r\n with spaces for better recognition.
+        private void ParseWords(string text)
+        {
+            //We could also limit the parallelism here for testing -> new ParallelOptions { MaxDegreeOfParallelism = 1 }
+            Parallel.ForEach(CreateChunks(text, 100), chunkBlock =>
+            {   
+                //split the block into words
+                chunkBlock = chunkBlock.Replace(Environment.NewLine, " ");
+                CleanUpWord(chunkBlock.Split(' '));
+            });
+
+            //After a collection has been marked as complete for adding, adding to the collection is 
+            //not permitted and attempts to remove from the collection will not wait when the collection is empty.
+            _wordChunks.CompleteAdding();
+        }
+
+        //This will add or update the dictionary with the number of references.
+        //It will use a thread safe delegate to increment the value by 1 otherwise it will be defaulted to 1.
+        private void UpdateDictionary()
+        {
+            Parallel.ForEach(_wordChunks.GetConsumingEnumerable(), word => { _distinctWordList.AddOrUpdate(word, 1, (key, oldValue) => Interlocked.Increment(ref oldValue)); });
+        }
+
+        //Main method to reduce the text and add all the words in a concurrent dictionary with a pair of key, value
+        //Each word will be recorded as key and when it's found, the value will be increased using a thread safe increment.
+        public void MapReduce(string text)
+        {  
+            //Create background process to map input data to words
+            ThreadPool.QueueUserWorkItem(delegate {
+                ParseWords(text);
+            });
+
+            //Update dictionary with key, value
+            UpdateDictionary();
+        }
+
+        //Method for display purposes using a sorted list.
+        //Also to count the whole list of words.
+        //This is just for testing and display
+        public StringBuilder SortedResults()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, int> kvp in _distinctWordList.OrderBy(key => key.Key))
+            {
+                sb.AppendLine(kvp.Key + ": " + kvp.Value);
+                Numwords += kvp.Value;
+            }
+            return sb;
+        }
+
+    }
+}
---
+++ src/CountingWordsConsole/Program.cs
@@ -0,0 +1,73 @@
+﻿//The MIT License (MIT)
+
+//Copyright (c) 2015 Jordi Corbilla
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+//######################################
+// Counting words using MapReduce Approach 
+// http://en.wikipedia.org/wiki/MapReduce
+// @Author: Jordi Corbilla
+// @version: 1.0
+// @date: 23/03/2015
+//######################################
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace CountingWordsConsole
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter the file you want to process.");
+                Console.WriteLine("Usage: CountingWordsConsole <filename>");
+                return 1;
+            }
+
+            Reducer reducer = new Reducer();
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                string readText = File.ReadAllText(args[0]);
+                Console.WriteLine("Starting reduction");
+                sw.Start();
+                reducer.MapReduce(readText);
+                sw.Stop();
+                Console.WriteLine("Reduction completed");
+                Console.WriteLine("Elapsed={0}", sw.Elapsed);
+                File.WriteAllText("Results.txt", reducer.SortedResults().ToString());
+                Console.WriteLine("Done!, processing {0:D} words", reducer.Numwords);
+                Console.WriteLine("Please review Results.txt");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+               
+            return 0;
+        }
+    }
+   
+}
